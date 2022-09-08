aaf6db921b1ad44f2b828d8c1d1324e3cb9af6cb
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-08 13:42:50 +0100

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	
+
 	return (0);
 }
-
