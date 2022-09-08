76a60c496f6ecab3ec11d504a1f22a156104613e
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-08 12:53:50 +0100

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main() - program start and ends in this function
+ * takes no arguments
+ *
+ * Description: contains a puts() function to print string to standard output
+ * Return: function returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
