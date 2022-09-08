9589a5e105e3b48819ce883fe0ba2eb5b28f8800
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-08 13:00:49 +0100

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program starts and ends in main
+ * main() takes no arguments
+ * Description: prints string to standard output using printf() funtion
+ * Return: main returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
