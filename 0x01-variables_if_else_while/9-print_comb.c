80643a942482015fb0ba8cee62d726a29396afbb
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-10 01:57:34 +0100

    optimized 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - entry point of program
+ * main - entry point
  *
- * Description: prints single-digit numbers seperated by space and comma,
- * and end in a newline.
+ * Description: prints 0 to 9 seperated by a comma and space
  * Return: 0 on success.
  */
 int main(void)
 {
 	int i = 48;
 
-	while (i >= 48 && i <= 57)
+	while (i <= 57)
 	{
 		putchar(i);
 		if (i == 57)
 			break;
-		putchar(44); /* ascii comma */
-		putchar(32); /* ascii space */
+		putchar(44);
+		putchar(32);
 		i++;
 	}
 	putchar(10);
