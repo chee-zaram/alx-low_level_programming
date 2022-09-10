e8737397d28307c5b09e4f53eae824c81434301a
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-10 01:15:24 +0100

    optimized task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - entry point of program
+ * main - entry point
  *
- * Description: prints all single digits of base 10 from 0 using only putchar()
- * Return: returns 0 on success
+ * Description: print all single digit numbers in base 10
+ * Using only putchar function, not more than twice
+ * Return: 0
  */
 int main(void)
 {
-	int i = 0;
-	int num_asc = 48;
+	int i = 48;
 
-	while (i >= 0)
+	while (i <= 57)
 	{
-		if (i < 10)
-		{
-			putchar(num_asc); /* ascii */
-			i++;
-			num_asc++;
-		}
-		else
-			break;
+		putchar(i);
+		i++;
 	}
-	putchar(10); /* newline ascii */
+	putchar(10);
 	return (0);
 }
 
