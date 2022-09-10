571746c34f85bcc55ff81683c198e25620e16500
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-10 03:03:48 +0100

    optimized 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - entry
  *
- * Description: prints all possible different combinations of three digits.
- * Numbers seperated by comma and space,
- * three digits being different,
- * combination only appearing once in form of the smallest number,
- * using only putchar function, max of 6 times,
- * no use of char data type.
- * Return: value of 0 on success.
+ * Description: nill
+ * Return: 0
  */
 int main(void)
 {
-	int num1, num2, num3;
+	int digit1, digit2, digit3;
 
-	for (num1 = 48; num1 < 56; num1++) /**
-					    * Incrementing num1 by 1 starting from 0.
-					    * Using ascii values.
-					    */
+	digit1 = 48;
+	while (digit1 <= 55)
 	{
-		for (num2 = num1 + 1; num2 < 57; num2++) /**
-							  * Initializing num2 as num1+1...
-							  * for each iteration of outermost loop.
-							  * Incrementing num2 by 1 starting from 1.
-							  */
-
+		digit2 = digit1 + 1;
+		while (digit2 <= 56)
 		{
-			for (num3 = num2 + 1; num3 <= 57; num3++) /**
-								   * Initializing num3 as num2+1...
-								   * for each iteration of outer loop.
-								   * Incrementing num3 by 1 starting from 3.
-								   */
+			digit3 = digit2 + 1;
+			while (digit3 <= 57)
 			{
-				putchar(num1);
-				putchar(num2);
-				putchar(num3);
-				if (num1 == 55 && num2 == 56 && num3 == 57)
-					continue;
+				putchar(digit1);
+				putchar(digit2);
+				putchar(digit3);
+				if (digit1 == 55 && digit2 == 56 && digit3 == 57)
+					break;
 				putchar(44);
 				putchar(32);
+				digit3++;
 			}
+			digit2++;
 		}
+		digit1++;
 	}
 	putchar(10);
 	return (0);
