cdb656a1eec16de0c3edc3a3ed7eb0bba6051e9d
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-13 09:48:27 +0100

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * Takes no arguments.
+ *
+ * Description: prints a string followed by a newline using _putchar function
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# C - Functions, Nested loops
+Here, codes that contain function declarations and definitions, and make use of loops within loops are written.
