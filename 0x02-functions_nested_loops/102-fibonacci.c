8ffa235e507668018a85304affc2f7f3145b0954
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-13 18:05:00 +0100

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program.
+ * Takes no arguments.
+ *
+ * Description: Prints the first 50 numbers of the Fibonacci sequence.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	unsigned long int i, x, y, z;
+
+	x = 1, y = 2;
+	for (i = 1; i < 51; i++)
+	{
+		if (x < 20365011074)
+			printf("%lu, ", x);
+		if (x == 20365011074)
+			printf("%lu\n", x);
+		z = x + y;
+		x = y;
+		y = z;
+	}
+	return (0);
+}
+
