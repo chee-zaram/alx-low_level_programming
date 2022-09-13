4a6e4707aa3e12319a2fba5604e72c56e55d2ba7
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-13 15:26:38 +0100

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @x: Value of first integer.
+ * @y: Value of second integer.
+ *
+ * Return: Sum of two integers.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
