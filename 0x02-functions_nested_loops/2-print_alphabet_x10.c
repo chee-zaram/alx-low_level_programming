48a973906c989570992f67181caf4f9d4229341b
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-13 10:34:42 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times on newline.
+ * Takes no arguments.
+ *
+ * Return: 1 on success. -1 on failure.
+ */
+void print_alphabet_x10(void)
+{
+	int x, ch;
+
+	x = 1;
+	while (x <= 10)
+	{
+		ch = 97;
+		while (ch <= 122)
+		{
+			_putchar(ch);
+			ch++;
+		}
+		x++;
+		_putchar(10);
+	}
+}
+
