99bd9ff793be93fb006ea15fca180e6d37cdd35c
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-13 17:31:02 +0100

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program.
+ * Takes no argument values.
+ *
+ * Description: Computes and prints the sum of all the multiples of 3 or 5
+ * which are below 1023, excluding 1023.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1023; i++)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+			sum = sum + i;
+		else
+			continue;
+	}
+	printf("%d\n", sum);
+}
+
