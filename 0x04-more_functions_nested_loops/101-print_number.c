13d7afe735238f70d8763bd00150043212e92e98
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-15 20:50:48 +0100

    Optimized Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int _abs(int x);
-
 /**
  * print_number - Prints an integer.
  * @n: Integer to be printed.
@@ -12,46 +10,19 @@ void print_number(int n)
 {
 	int abs_v;
 
-	if (n >= 0 && n <= 9)
-		_putchar(n + 48);
-	else if (n >= 10 && n <= 99)
+	if (n >= 0)
 	{
-		_putchar((n / 10) + 48);
-		_putchar((n % 10) + 48);
+		abs_v = n;
 	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar(((n / 10) / 10) + 48);
-		_putchar(((n / 10) % 10) + 48);
-		_putchar((n % 10) + 48);
-	}
-	else if (n >= 1000 && n <= 9999)
-	{
-		_putchar((((n / 10) / 10) / 10) + 48);
-		_putchar((((n / 10) / 10) % 10) + 48);
-		_putchar(((n / 10) % 10) + 48);
-		_putchar((n % 10) + 48);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		abs_v = _abs(n);
-		_putchar((abs_v / 10) + 48);
-		_putchar((abs_v % 10) + 48);
-	}
-}
-
-/**
- * _abs - Computes the absolute value of an integer.
- * @x: Integer to be operated on.
- *
- * Return: Absolute value of x.
- */
-int _abs(int x)
-{
-	if (x < 0)
-		return (x * -1);
 	else
-		return (x);
+	{
+		abs_v = n * -1;
+		_putchar('-');
+	}
+	if (abs_v / 10)
+	{
+		print_number(abs_v / 10);
+	}
+	_putchar((abs_v % 10) + 48);
 }
 
