3debf314d112757267656e8ca95ecd51acbee457
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-19 10:40:32 +0100

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Takes a pointer to an int as parameter and updates the value.
- * @*n - Argument.
+ * @n - Argument.
  *
  * Description: Updates the value it points to to 98.
  * Return: Unspecified return value.
