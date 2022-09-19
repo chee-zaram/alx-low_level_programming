83b508f1bb6470c8ee0cd71fd33e5c1c3d1ab8f5
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-19 15:10:17 +0100

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: Pointer to string.
+ *
+ * Return: No specified return value.
+ */
+void rev_string(char *s)
+{
+	char r_s[10];
+	int i, j, k;
+
+	j = 0;
+	for (i = _strlen(s) - 1; i >= 0; i--)
+	{
+		r_s[j] = s[i];
+		j++;
+	}
+	for (i = 0; 1 ; i++)
+	{
+		if (r_s[i] == 0)
+			break;
+		s[i] = r_s[i];
+	}
+}
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Argument pointer of char type.
+ *
+ * Return: Length of a string.
+ */
+int _strlen(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; 1 ; i++) /* 1 says if True i.e s still exists */
+	{
+		if (*(s + i) == 0)
+			break;
+		len++;
+	}
+	return (len);
+}
+
