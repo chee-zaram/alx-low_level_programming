a98d1ab14029fdcfd60ea331c2dc322b9228bf78
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-19 16:01:56 +0100

    Increased array size

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,39 +8,26 @@
  */
 void rev_string(char *s)
 {
-	int i, j;
-	char r_s[_strlen(s) + 1];
+	int i, j, k;
+	char revString[500];
 
-	j = 0;
-	for (i = _strlen(s) - 1; i >= 0; i--)
+	k = 0;
+	for (i = 0; 1 ; i++)
 	{
-		r_s[j] = s[i];
-		j++;
+		if (s[i] == 0)
+			break;
+		k++;
+	}
+	i = 0;
+	for (j = k - 1; j >= 0; j--)
+	{
+		revString[i] = s[j];
+		i++;
 	}
 	for (i = 0; 1 ; i++)
 	{
-		if (*(r_s + i) == 0)
+		if (revString[i] == 0)
 			break;
-		s[i] = r_s[i];
+		s[i] = revString[i];
 	}
 }
-
-/**
- * _strlen - Returns the length of a string.
- * @s: Argument pointer of char type.
- *
- * Return: Length of a string.
- */
-int _strlen(char *s)
-{
-	int i, len = 0;
-
-	for (i = 0; 1 ; i++) /* 1 says if True i.e s still exists */
-	{
-		if (*(s + i) == 0)
-			break;
-		len++;
-	}
-	return (len);
-}
-
