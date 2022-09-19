b6e2730bbd39dd9ef75a347070b2c8a6b6d04a34
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-19 10:39:40 +0100

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter and updates the value.
+ * @*n - Argument.
+ *
+ * Description: Updates the value it points to to 98.
+ * Return: Unspecified return value.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# C - Pointers, arrays, and strings
+Programs that fundamentally make use of arrays, pointers, and strings with the aim of solidifying the concepts in memory.
