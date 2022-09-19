545e04475df40226f06571d0d5760fdcdee7a1d1
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-19 13:32:41 +0100

    Task 3 and corrected typo is Task 2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: Pointer to string.
+ *
+ * Return: No specified return value.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
+
+/**
  * _strlen - Returns the length of a string.
  * @s: Argument pointer of char type.
  *
