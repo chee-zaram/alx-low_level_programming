e3534948456be216a14232fc219de5be696eeac2
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-19 17:36:57 +0100

    Fixed error for invalid array sizes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n != 0 && n != 1)
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
@@ -23,6 +23,10 @@ void print_array(int *a, int n)
 			}
 			printf("%d, ", a[i]);
 		}
+	}
+	else
+	{
+		printf("\n");
 	}
 }
 
