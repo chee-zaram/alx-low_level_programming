f2a0fc6b7c42c3a6ac9c68dc4f53ceb01eb34bed
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-19 11:14:03 +0100

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the lenght of a string.
+ * @s: Argument pointer of char type.
+ *
+ * Return: Length of a string.
+ */
+int _strlen(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; i <= s[i]; i++)
+	{
+		len++;
+	}
+	return (len);
+}
+
