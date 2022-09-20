4a620e72a8c24af2a1df71aaaead29ea75f40706
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-20 06:13:37 +0100

    Task 12

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,36 +16,29 @@ int main(void)
 	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char lower[] = "abcdefghijklmnopqrstuvwxyz";
 	char nums[] = "0123456789";
-	char special_char[] = "!@#$^&*?";
 	char passwrd[100]; /* length of password */
 
 	srand((unsigned int)(time(NULL))); /* to generate new numbers each time */
-	random = rand() % 4;
+	random = rand() % 3;
 
 	for (i = 0; i < 100; i++)
 	{
 		if (random == 1)
 		{
 			passwrd[i] = nums[rand() % 10]; /* pick randomly from 10 nums */
-			random = rand() % 4;
+			random = rand() % 3;
 			printf("%c", passwrd[i]);
 		}
 		else if (random == 2)
 		{
-			passwrd[i] = special_char[rand() % 8]; /* randomly select 8 character */
-			random = rand() % 4;
-			printf("%c", passwrd[i]);
-		}
-		else if (random == 3)
-		{
 			passwrd[i] = upper[rand() % 26]; /* pick randomly from 26 capital letters */
-			random = rand() % 4;
+			random = rand() % 3;
 			printf("%c", passwrd[i]);
 		}
 		else
 		{
 			passwrd[i] = lower[rand() % 26]; /* pick randomly from 26 small letters */
-			random = rand() % 4;
+			random = rand() % 3;
 			printf("%c", passwrd[i]);
 		}
 	}
