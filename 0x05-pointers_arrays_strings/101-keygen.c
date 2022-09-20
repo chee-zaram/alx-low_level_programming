0847bb702e087d8eb725107efbc0579a0f094a38
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-20 05:57:37 +0100

    Task 11 for generating a random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - Entry point of program.
+ * Takes no arguments.
+ *
+ * Description: Generates a valid random password.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	int i, random = 0;
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	char nums[] = "0123456789";
+	char special_char[] = "!@#$^&*?";
+	char passwrd[20]; /* length of password */
+
+	srand((unsigned int)(time(NULL))); /* to generate new numbers each time */
+	random = rand() % 4;
+
+	for (i = 0; i < 20; i++)
+	{
+		if (random == 1)
+		{
+			passwrd[i] = nums[rand() % 10]; /* pick randomly from 10 nums */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+		else if (random == 2)
+		{
+			passwrd[i] = special_char[rand() % 8]; /* randomly select 8 character */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+		else if (random == 3)
+		{
+			passwrd[i] = upper[rand() % 26]; /* pick randomly from 26 capital letters */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+		else
+		{
+			passwrd[i] = lower[rand() % 26]; /* pick randomly from 26 small letters */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+	}
+	return (0);
+}
+
