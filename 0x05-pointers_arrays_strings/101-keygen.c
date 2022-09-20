2303209872ce030adb58574695ca34d141fc540b
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-20 06:08:02 +0100

    Task 12

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- * Return: Returns 0
+ * main - Entry point of program.
+ * Takes no arguments.
+ *
+ * Description: Generates a valid random password.
+ * Return: 0 if successful.
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+	int i, random = 0;
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	char nums[] = "0123456789";
+	char special_char[] = "!@#$^&*?";
+	char passwrd[20]; /* length of password */
 
-	sum = 0;
-	srand(time(NULL));
+	srand((unsigned int)(time(NULL))); /* to generate new numbers each time */
+	random = rand() % 4;
 
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if (random == 1)
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
+			passwrd[i] = nums[rand() % 10]; /* pick randomly from 10 nums */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+		else if (random == 2)
+		{
+			passwrd[i] = special_char[rand() % 8]; /* randomly select 8 character */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+		else if (random == 3)
+		{
+			passwrd[i] = upper[rand() % 26]; /* pick randomly from 26 capital letters */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+		else
+		{
+			passwrd[i] = lower[rand() % 26]; /* pick randomly from 26 small letters */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
 		}
 	}
 	return (0);
 }
+
