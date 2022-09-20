16b7376351105ee419ad8351d67691cf50836f27
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-20 06:24:03 +0100

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,29 +16,36 @@ int main(void)
 	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char lower[] = "abcdefghijklmnopqrstuvwxyz";
 	char nums[] = "0123456789";
+	char s_c[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 	char passwrd[100]; /* length of password */
 
 	srand((unsigned int)(time(NULL))); /* to generate new numbers each time */
-	random = rand() % 3;
+	random = rand() % 4;
 
 	for (i = 0; i < 100; i++)
 	{
 		if (random == 1)
 		{
 			passwrd[i] = nums[rand() % 10]; /* pick randomly from 10 nums */
-			random = rand() % 3;
+			random = rand() % 4;
 			printf("%c", passwrd[i]);
 		}
 		else if (random == 2)
 		{
+			passwrd[i] = s_c[rand() % 32]; /* pick random s c */
+			random = rand() % 4;
+			printf("%c", passwrd[i]);
+		}
+		else if (random == 3)
+		{
 			passwrd[i] = upper[rand() % 26]; /* pick randomly from 26 capital letters */
-			random = rand() % 3;
+			random = rand() % 4;
 			printf("%c", passwrd[i]);
 		}
 		else
 		{
 			passwrd[i] = lower[rand() % 26]; /* pick randomly from 26 small letters */
-			random = rand() % 3;
+			random = rand() % 4;
 			printf("%c", passwrd[i]);
 		}
 	}
