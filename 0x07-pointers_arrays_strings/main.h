3b6e776b942f9922867e982068bb198edb0f854f
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-26 14:05:46 +0100

    Updated main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
+void print_diagsums(int *a, int size);
 
 #endif /* for MAIN_H */
