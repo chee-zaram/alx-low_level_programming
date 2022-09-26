bd18d0128247560132ed8ac1f759cdd57d1d79e6
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-26 11:13:05 +0100

    Task 3 and updated main.h

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the string to be examined.
+ * @accept: Pointer to accepted characters of substring.
+ *
+ * Return: Returns number of bytes in initial segment of s,
+ * consisting only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+	unsigned int count = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count += 1;
+				for (k = 0; accept[k]; k++)
+				{
+					if (s[i + 1] == accept[k])
+						break;
+					else if (accept[k + 1] == 0)
+						return (count);
+				}
+			}
+		}
+	}
+}
+
