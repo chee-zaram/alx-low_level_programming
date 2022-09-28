63983f5328fc6916cc78c47154f622047d901c2d
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-28 13:12:30 +0100

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Computes if integer input is a prime number.
+ * @n: Integer to be checked.
+ *
+ * Return: Returns 1 if prime, 0 if not.
+ */
+int is_prime_number(int n)
+{
+	int p = 2;
+
+	if (n == 0 || n == 1 || n == -1)
+		return (0);
+	else
+		return (check_prime(n, p));
+}
+
+/**
+ * check_prime - Checks if numbers other 0, 1, -1 is prime.
+ * @n: Number to check.
+ * @p: Potential factor.
+ *
+ * Return: 1 if prime, 0 if not.
+ */
+int check_prime(int n, int p)
+{
+	if (n % p == 0 && n != p)
+		return (0);
+	else if (n % p != 0 && p < n)
+		return (check_prime(n, p + 1));
+	else
+		return (1);
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
+int is_prime_number(int n);
+int check_prime(int n, int p);
 
 #endif /* for MAIN_H */
