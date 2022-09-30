d50043d2b8a990f52e87ffd120e534d3c38ec32d
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-09-30 10:03:45 +0100

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
+/**
+ * main - Entry point of program.
+ * @argc: Number of arguments passed to main(), including program name.
+ * @argv: Pointer to array of strings, each being an argument passed to main.
+ *
+ * Description: Prints the program name, that is first argument.
+ * Return: 0 if successful.
+ */
 int main(int argc, char **argv)
 {
 	if (1)
 		printf("%s\n", *argv);
-	exit(EXIT_SUCCESS);
+	return (0);
 }
 
