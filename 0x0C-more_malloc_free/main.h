5d28d784a1d595289838f152fca7128216889ed4
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-06 16:43:47 +0100

    More malloc() and free()

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,8 +11,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void print_num(long long num);
-void _puts(char *s, short len);
-int _putchar(char c);
 
 #endif /* for MAIN_H */
