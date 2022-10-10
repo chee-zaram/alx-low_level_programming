1945e28424a21cbc26bb201ef1016fbc30a5cc59
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-10 13:21:51 +0100

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,8 +10,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new = malloc(sizeof(dog_t));
+	dog_t *new;
 
+	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
 
