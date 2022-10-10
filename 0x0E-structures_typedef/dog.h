01042afed2df59e82d57e78a2ce8fc9ac8890bbb
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-10 11:45:50 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - Details of a dog.
@@ -16,7 +17,14 @@ struct dog
 	char *owner;
 };
 
+/*
+ * dog_t - Synonym for struct dog.
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* for DOG_H */
