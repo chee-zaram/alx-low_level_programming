5e0e735358b01b2b78ec6aa704084dfb2835744f
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-13 22:09:59 +0100

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VAR_FUNCTIONS
+#define VAR_FUNCTIONS
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* for VAR_FUNCTIONS */
