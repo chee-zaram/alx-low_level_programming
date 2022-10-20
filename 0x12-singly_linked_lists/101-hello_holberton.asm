98df6821022758bab4e7c86d45c00d43ca19e675
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-20 20:37:06 +0100

    64bit program in assembly to print a string

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,15 +6,12 @@ main:
 	push	rbp
 	mov	rbp,rsp
 	sub	rsp,16
-
 	xor	eax,eax
 	lea	rdi,[msg]
 	call	printf
-
-	add rsp,16
+	add	rsp,16
 	leave
 	ret
 
 section	.data
-msg: 
-	db "Hello",' ',"Holberton",10,0
+msg:	db "Hello Holberton",10,0
