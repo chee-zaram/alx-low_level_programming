3a2699adfe20c6992792cd857354cd4c8b6f507e
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-20 16:49:27 +0100

    Printing a string before main() is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * first - Function that prints a string before main() is executed.
+ *
+ * Return: No return value.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
