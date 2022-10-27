b36721aec167ba0df9ec48a99c13dee84d286b42
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-27 13:18:44 +0100

    Add function to set the bit at a specified index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the bit at a given index.
+ * @n: Pointer to the number for which to manipulate bit.
+ * @index: Index of bit to be manipulated.
+ *
+ * Return: 1 if successful, -1 if not.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index <= 63)
+	{
+		mask = 1 << index;
+		*n = mask | *n;
+		return (1);
+	}
+	return (-1);
+}
+
