b84eaee5cafe2346f37636643e7d9d1fc159757c
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-27 12:28:18 +0100

    Add function to print the binary representation of any number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,14 +11,3 @@ void print_binary(unsigned long int n)
 	_putchar((n & 1) + 48);
 }
 
-/**
- * _putchar - Writes a character to the standard output.
- * @c: Character to write.
- *
- * Return: 1, or -1 if failed.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
