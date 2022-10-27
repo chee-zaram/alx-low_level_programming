32ede22e05b81246d3f353e642d4cea209a0d47c
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-27 12:54:44 +0100

    Add function to get the bit at a specified index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - Gets the bit at a specified index.
+ * @n: Number in which to check bit.
+ * @index: Index of bit to get.
+ *
+ * Return: The value of the bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int mask = 1 << index;
+
+	if (n & mask)
+		return (1);
+	else if (!(n & mask))
+		return (0);
+	return (-1);
+
+}
+
