71ac3999b149e820f28eb9cb29b7045c791d6608
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-31 15:06:52 +0100

    Add function prototype for appending to file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,8 +7,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* for _MAIN_H_ */
