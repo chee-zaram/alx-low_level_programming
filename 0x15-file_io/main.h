14bc3f70fe0743c556e5d309c55aa67cf6e391b2
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-10-31 14:13:25 +0100

    Add function prototype

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* for _MAIN_H_ */
