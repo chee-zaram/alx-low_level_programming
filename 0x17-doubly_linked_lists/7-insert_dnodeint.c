c5fac7f71530bbb46d792beb7c06f798b9f4e580
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-12-09 14:28:26 +0100

    0x17: Refactor code to use iteration as no fix for bug in recursion yet

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,33 +10,31 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL;
+	dlistint_t *new = NULL, *temp = NULL;
+	unsigned int i = 0;
 
 	if (h)
 	{
 		if (idx == 0)
-		{
-			new = malloc(sizeof(dlistint_t));
-			if (!new)
-				return (new);
+			return (add_dnodeint(h, n));
 
-			new->prev = NULL;
-			new->next = NULL;
-			new->n = n;
-			if (*h)
+		for (i = 0, temp = *h; temp && i < idx; temp = temp->next, i++)
+			if (i == idx - 1)
 			{
-				if (!(*h)->prev)
+				if (temp->next)
 				{
-					(*h)->prev = new;
-					new->next = (*h);
+					new = malloc(sizeof(*new));
+					if (!new)
+						return (new);
+					new->n = n;
+					new->next = temp->next;
+					new->prev = temp;
+					temp->next->prev = new;
+					temp->next = new;
+					return (new);
 				}
-				new->next = *h;
-				(*h)->prev = new;
+				return (add_dnodeint_end(h, n));
 			}
-			*h = new;
-		}
-		else if (*h)
-			return (insert_dnodeint_at_index(&(*h)->next, idx - 1, n));
 	}
 	return (new);
 }
