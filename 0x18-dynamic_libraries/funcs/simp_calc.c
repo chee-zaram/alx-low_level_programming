1acc85ebe36e439d6c84ed9235d447be1407ca9d
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-12-12 11:22:44 +0100

    0x18: Define function to perform basic arithmetic

---
+++ 0x18-dynamic_libraries/funcs/simp_calc.c
@@ -0,0 +1,27 @@
+/**
+ * Functions to perform basic arithmetics.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
