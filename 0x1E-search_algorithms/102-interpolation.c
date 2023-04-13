93e31409c58fd57e91d83bc421b8cf505c8fa76d
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-04-13 18:59:35 +0100

    feat(102-interpolation.c): implement interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Finds a value in a sorted array using the
+ *		interpolation algorithm.
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index of the value, or -1 if it could not be found.
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t beg = 0, end = size - 1, pos = 0;
+
+	if (!array || size < 1)
+		return (NOT_FOUND);
+
+	while (beg <= end && value >= array[beg] && value <= array[end])
+	{
+		pos = INTERPOLATION_FORMULA;
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] < value)
+		{
+			beg = pos + 1;
+			continue;
+		}
+
+		if (array[pos] > value)
+		{
+
+			end = pos - 1;
+			continue;
+		}
+
+		/* Check if it's the first occurence of the value */
+		while (pos > 0 && array[pos - 1] == value)
+		{
+			pos--;
+			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		}
+		return (pos);
+	}
+
+	printf("Value checked array[%lu] is out of range\n", beg);
+	return (NOT_FOUND);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NOT_FOUND -1
+#define DIFF      (double)(end - beg)
+#define INTERPOLATION_FORMULA \
+	(beg + ((DIFF / (array[end] - array[beg])) * (value - array[beg])))
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* for SEARCH_ALGOS */
