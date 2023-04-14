5b409817d655914cb0bc4ef9a0646d687e3b3ac1
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-04-14 10:18:39 +0100

    refactor: get rid of redundant `search` function

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -15,36 +15,6 @@ static void print_array(int *array, size_t l_bound, size_t u_bound)
 }
 
 /**
- * search - Seareches for a value in an array using binary search algorithm
- * @array: Array to be searched
- * @l_bound: Lower bound of the array
- * @u_bound: Upper bound of the array
- * @value: Value to be searched for
- *
- * Return: Index of first occurrence of value, otherwise NOT_FOUND
- */
-static int search(int *array, size_t l_bound, size_t u_bound, int value)
-{
-	size_t mid    = 0;
-	int    result = NOT_FOUND;
-
-	if (l_bound > u_bound)
-		return (NOT_FOUND);
-
-	mid = (l_bound + u_bound) / 2;
-	print_array(array, l_bound, u_bound);
-
-	if (array[mid] == value && (mid == 0 || array[mid - 1] != value))
-		result = mid;
-	if (array[mid] >= value)
-		result = search(array, l_bound, mid, value);
-	else
-		result = search(array, mid + 1, u_bound, value);
-
-	return (result);
-}
-
-/**
  * advanced_binary - Finds first occurrence of a value in an array
  * @array: An array containing items
  * @size: The number of items in the array
@@ -57,9 +27,28 @@ static int search(int *array, size_t l_bound, size_t u_bound, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
+	size_t mid   = 0;
+	int    index = 0;
 
 	if (!array || size < 1)
 		return (NOT_FOUND);
 
-	return (search(array, 0, size - 1, value));
+	print_array(array, 0, size - 1);
+
+	if (size == 1 && array[0] != value)
+		return (NOT_FOUND);
+
+	mid = (size - 1) / 2;
+	if (array[mid] == value && (mid == 0 || array[mid - 1] != value))
+		return (mid);
+
+	if (array[mid] < value)
+	{
+		index = advanced_binary(array + mid + 1, size - mid - 1, value);
+		if (index == NOT_FOUND)
+			return (NOT_FOUND);
+		return (index + mid + 1);
+	}
+
+	return (advanced_binary(array, mid + 1, value));
 }
